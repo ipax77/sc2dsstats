0b5a274db3cc6b46b89b279abbc1ee820290ceb9
ipax77 <ph.ilipp@web.de>
2020-02-29 14:54:11 +0100

    pickban
    Signed-off-by: ipax77 <ph.ilipp@web.de>

---
+++ pickban/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace pickban
+{
+    public class Program
+    {
+
+        public static string[] s_races_cmdr { get; } = new string[]
+        {
+                "Abathur",
+                 "Alarak",
+                 "Artanis",
+                 "Dehaka",
+                 "Fenix",
+                 "Horner",
+                 "Karax",
+                 "Kerrigan",
+                 "Nova",
+                 "Raynor",
+                 "Stetmann",
+                 "Stukov",
+                 "Swann",
+                 "Tychus",
+                 "Vorazun",
+                 "Zagara"
+        };
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
