a04116ccf3bed2c4feffc205429851e30545cc96
ipax77 <ph.ilipp@web.de>
2022-10-14 16:11:53 +0200

    MMrService

---
+++ pax.dsstats.dbng/Services/MmrService.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pax.dsstats.dbng.Services;
+
+public class MmrService
+{
+    private readonly ReplayContext context;
+
+    public MmrService(ReplayContext context)
+    {
+        this.context = context;
+    }
+
+    private Dictionary<int, double> toonIdRatings = new();
+
+
+    public async Task CalcMmmr()
+    {
+        toonIdRatings.Clear();
+
+        var replays = context.Replays
+            .Include(i => i.Players)
+                .ThenInclude(i => i.Player)
+            .Where(x => x.Duration >= 300 && x.Maxleaver < 89 && x.WinnerTeam > 0)
+            .Where(x => x.GameMode == shared.GameMode.Commanders)
+            .OrderBy(o => o.GameTime)
+            .AsNoTracking();
+
+
+        await replays.ForEachAsync(f =>
+        {
+            var winnerTeamIds = f.Players.Where(x => x.Team == f.WinnerTeam).Select(m => m.Player.ToonId);
+            var runnerTeamIds = f.Players.Where(x => x.Team != f.WinnerTeam).Select(m => m.Player.ToonId);
+
+            var winnerTeamMmr = GetTeamMmr(winnerTeamIds);
+            var runnerTeamMmr = GetTeamMmr(runnerTeamIds);
+
+            var delta = CalculateELODelta(winnerTeamMmr, runnerTeamMmr);
+
+            SetWinnerMmr(winnerTeamIds, delta);
+            SetRunnerMmr(runnerTeamIds, delta);
+        });
+
+        foreach (var ent in toonIdRatings.OrderByDescending(o => o.Value).Take(15))
+        {
+            var dbPl = await context.Players.FirstAsync(f => f.ToonId == ent.Key);
+            Console.WriteLine($"{dbPl.Name}|{ent.Key} => {ent.Value:N2}");
+        }
+    }
+
+    private void SetRunnerMmr(IEnumerable<int> runnerTeamIds, double delta)
+    {
+        foreach (var id in runnerTeamIds)
+        {
+            toonIdRatings[id] -= delta;
+        }
+    }
+
+    private void SetWinnerMmr(IEnumerable<int> winnerTeamIds, double delta)
+    {
+        foreach (var id in winnerTeamIds)
+        {
+            toonIdRatings[id] += delta;
+        }
+    }
+
+    private static double ExpectationToWin(double playerOneRating, double playerTwoRating)
+    {
+        return 1.0 / (1.0 + Math.Pow(10.0, (playerTwoRating - playerOneRating) / 400.0));
+    }
+
+    private static double CalculateELODelta(double winnerTeamMmr, double runnerTeamMmr)
+    {
+        int eloK = 32;
+
+        return (double)(eloK * (1.0 - ExpectationToWin(winnerTeamMmr, runnerTeamMmr)));
+    }
+
+    private double GetTeamMmr(IEnumerable<int> playerToonIds)
+    {
+        double teamMmr = 0;
+
+        foreach (var playerToonId in playerToonIds) {
+            if (!toonIdRatings.ContainsKey(playerToonId))
+            {
+                toonIdRatings[playerToonId] = 1000;
+                teamMmr += 1000;
+            }
+            else
+            {
+                teamMmr += toonIdRatings[playerToonId];
+            }
+        }
+        return teamMmr;
+    }
+
+}
--- sc2dsstats.test/Program.cs
+++ sc2dsstats.test/Program.cs
@@ -1,7 +1,37 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using pax.dsstats.dbng;
+using pax.dsstats.dbng.Services;
+
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 var loc = Assembly.GetExecutingAssembly().Location;
 
 Console.WriteLine($"Location: {loc}");
+
+var services = new ServiceCollection();
+
+services.AddDbContext<ReplayContext>(options =>
+{
+    options.UseSqlite(@"Data Source=C:\Users\pax77\AppData\Local\Packages\sc2dsstats.maui_veygnay3cpztg\LocalState\dsstats2.db",
+        x =>
+        {
+            x.MigrationsAssembly("SqliteMigrations");
+            x.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+        }
+        );
+});
+
+var serviceProvider = services.BuildServiceProvider();
+var context = serviceProvider.GetService<ReplayContext>();
+
+ArgumentNullException.ThrowIfNull(context);
+
+var mmrService = new MmrService(context);
+
+mmrService.CalcMmmr().GetAwaiter().GetResult();
+
+Console.ReadLine();
